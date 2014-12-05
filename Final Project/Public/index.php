23ed7759b50e67648ee638b8be57185bdf319353
inet2005 <w0242086@nscc.ca>
2014-12-05 02:47:56 +0000

    User controller added. Login and register functionality added. password hashing and salting is aOkay.

--- Final Project/Public/index.php
+++ Final Project/Public/index.php
@@ -9,12 +9,13 @@ $cssTemplate = 1;
 require_once '../Controller/cssController.php';
 require_once '../Controller/pageController.php';
 require_once'../Controller/contentAreaController.php';
-require_once '../Model/userModel.php';
+require_once '../Controller/userController.php';
 
 //$actorController = new Actor();
 $cssController = new CSS();
 $pageController = new Page();
 $contentAreaController = new ContentArea();
+$userController = new user();
 ?>
 
 <!DOCTYPE html>
@@ -69,13 +70,6 @@ $contentAreaController->displayContentAreaEditPage(1);
 //$userTest = new user();
 //$userArray = $userTest->retrieveUserInfo();
 //echo "";
-
-
-$um = new userModel();
-
-$um->register('testRegister', 'password', 'test', 'name', 1); //test, delete after.
-
-
 
 
 ?>
